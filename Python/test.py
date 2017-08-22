5a89cf4770819f8c0cd92e50e4fe89fcdd463e1b
Joost Zonneveld <joostz14@gmail.com>
2017-08-22 17:50:03 +0200

    n

---
+++ Python/test.py
@@ -0,0 +1,3 @@
+import os
+#os.system('""D:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/Common7/IDE/devenv.exe""')
+os.system('"C:/Windows/System32/notepad.exe"')
---
+++ Python/test1.py
@@ -0,0 +1,5 @@
+import subprocess
+
+
+subprocess.call("C:/Windows/System32/notepad.exe")
+subprocess.call("D:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/Common7/IDE/devenv.exe")

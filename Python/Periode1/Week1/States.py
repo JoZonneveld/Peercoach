e78eac37eee21310c75203975d6ab4747eb21955
Joost Zonneveld <joostz14@gmail.com>
2017-09-15 11:55:42 +0200

    Les 1 a

--- Python/Periode1/Week1/States.py
+++ Python/Periode1/Week1/States.py
@@ -8,20 +8,25 @@
 #AND V1 =TRUE  V2 = FALSE  == FALSE
 #AND V1 =FALSE  V2 = FALSE  == FALSE
 
-x = 1
-y = 5
+x = 3
+y = 7
 
-while x >= 0 and y >= 0:
+while x >= 0 or y >= 0:
   x = x - 1
-  y = y -2
+  y = y - 3
 
-# from random import randint
-#
-# #gives a random number
-# test = (randint(1, 3))
-#
-# # V = Variable : S = State
-# # Define V x : S = 10
+from random import randint
+
+
+
+
+
+
+#gives a random number
+test = (randint(1, 3))
+
+# V = Variable : S = State
+# Define V x : S = 10
 # x = 10
 #
 # print(x)

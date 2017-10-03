6dbb1d0a80a72001117a06fa167508b49de1fe35
JoZonneveld <joostz14@gmail.com>
2017-10-03 10:37:14 +0200

    opgeschoond

---
+++ Python/Periode1/Oefeningen/Calculator.py
@@ -0,0 +1,15 @@
+#create a calculator
+
+# i = input()
+
+getal1 = int(input("Getal 1: "))
+
+operator = input("Kies een operator + - / * ")
+
+getal2 = int(input("Getal 2: "))
+
+
+if operator == "+":
+    print(getal1 + getal2)
+elif operator == "-":
+    pass
---
+++ Python/Periode1/Oefeningen/square.py
@@ -0,0 +1,12 @@
+print("***\n***\n***")
+
+i = int(input("Voer een getal in: "))
+
+output = "";
+
+x = 0
+while x < i:
+    output += "*"
+    x += 1
+
+print (output)
